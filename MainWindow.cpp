e11bc4414903afe84377ebb135d0fb6ec1c55859
Maximo Pardo <122894192+maxipardo@users.noreply.github.com>
2026-07-26 18:24:28 -0300

    Download finished location and code style

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,177 +2,181 @@
 #include "Service.h"
 #include <qobject.h>
 
-MainWindow::MainWindow(QWidget *parent) 
-    : QMainWindow(parent)
-{
-    QWidget *centralWidget = new QWidget(this);
-    
-    layout = new QVBoxLayout(centralWidget);
-    linkLayout = new QHBoxLayout(centralWidget);
-    linkBox = new QLineEdit(centralWidget);
-    downloadButton = new QPushButton(centralWidget);
-    getEngineButton = new QPushButton(centralWidget);
-    statusLabel = new QLabel(centralWidget);
-    progressBar = new QProgressBar(centralWidget);
-    maintainer = new ServiceMaintainer(this);
-    chosenDirectory = QDir::homePath();
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+  QWidget *centralWidget = new QWidget(this);
 
-    /* Menu */
-    optionsMenu = new QMenu("Options", this);
-    buildMenu = new QMenu("Choose yt-dlp version", optionsMenu);
-    menuBar()->addMenu(optionsMenu);
-    optionsMenu->addMenu(buildMenu);
+  layout = new QVBoxLayout(centralWidget);
+  linkLayout = new QHBoxLayout();
+  linkBox = new QLineEdit(centralWidget);
+  downloadButton = new QPushButton(centralWidget);
+  getEngineButton = new QPushButton(centralWidget);
+  statusLabel = new QLabel(centralWidget);
+  progressBar = new QProgressBar(centralWidget);
+  maintainer = new ServiceMaintainer(this);
+  chosenDirectory = QDir::homePath();
 
-    chooseLocationAction = new QAction("Change download location...", this);
-    chooseNightlyAction = new QAction("Use yt-dlp nightly (recommended)", this);
-    chooseStableAction = new QAction("Use yt-dlp stable", this);
-    versionGroup = new QActionGroup(this);
+  /* Menu */
+  optionsMenu = new QMenu("Options", this);
+  buildMenu = new QMenu("Choose yt-dlp version", optionsMenu);
+  menuBar()->addMenu(optionsMenu);
+  optionsMenu->addMenu(buildMenu);
 
-    chooseNightlyAction->setCheckable(true);
-    chooseStableAction->setCheckable(true);
-    chooseNightlyAction->setChecked(true);
-    versionGroup->addAction(chooseNightlyAction);
-    versionGroup->addAction(chooseStableAction);
+  chooseLocationAction = new QAction("Change download location...", this);
+  chooseNightlyAction = new QAction("Use yt-dlp nightly (recommended)", this);
+  chooseStableAction = new QAction("Use yt-dlp stable", this);
+  versionGroup = new QActionGroup(this);
 
-    optionsMenu->addAction(chooseLocationAction);
-    buildMenu->addAction(chooseNightlyAction);
-    buildMenu->addAction(chooseStableAction);
+  chooseNightlyAction->setCheckable(true);
+  chooseStableAction->setCheckable(true);
+  chooseNightlyAction->setChecked(true);
+  versionGroup->addAction(chooseNightlyAction);
+  versionGroup->addAction(chooseStableAction);
 
-    linkBox->setPlaceholderText("Enter link...");
-    downloadButton->setText("Download link");
-    downloadButton->setEnabled(false);
-    getEngineButton->setText("Update yt-dlp");
-    layout->addLayout(linkLayout);
-    linkLayout->addWidget(linkBox);
-    linkLayout->addWidget(downloadButton);
-    layout->addWidget(statusLabel);
-    layout->addWidget(progressBar);
-    progressBar->hide();
-    layout->addWidget(getEngineButton);
+  optionsMenu->addAction(chooseLocationAction);
+  buildMenu->addAction(chooseNightlyAction);
+  buildMenu->addAction(chooseStableAction);
 
-    this->setWindowTitle("Phoca");
-    setCentralWidget(centralWidget);
+  linkBox->setPlaceholderText("Enter link...");
+  downloadButton->setText("Download link");
+  downloadButton->setEnabled(false);
+  getEngineButton->setText("Update yt-dlp");
+  layout->addLayout(linkLayout);
+  linkLayout->addWidget(linkBox);
+  linkLayout->addWidget(downloadButton);
+  layout->addWidget(statusLabel);
+  layout->addWidget(progressBar);
+  progressBar->hide();
+  layout->addWidget(getEngineButton);
 
-    /* Download location */
-    QFile file("config.txt");
+  this->setWindowTitle("Phoca");
+  setCentralWidget(centralWidget);
 
-    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        QTextStream entrada(&file);
-        downloadLocation = entrada.readAll().trimmed(); 
-        file.close();
-        qDebug() << "Download location found:" << downloadLocation;
-    } else {
-        downloadLocation = QStandardPaths::writableLocation(QStandardPaths::DownloadLocation);
-        qDebug() << "file not found. Using: " << downloadLocation;
-    }
+  /* Download location */
+  QFile file("config.txt");
 
-    connect(linkBox, &QLineEdit::textChanged, this, &MainWindow::setDownloadReadiness);
-    connect(getEngineButton, &QPushButton::clicked, this, &MainWindow::getServiceSlot); 
-    connect(maintainer, &ServiceMaintainer::started, this, &MainWindow::engineDownloading); 
-    connect(maintainer, &ServiceMaintainer::finished, this, &MainWindow::engineDownloaded); 
-    connect(chooseStableAction, &QAction::triggered, this, &MainWindow::getServiceSlot);
-    connect(chooseNightlyAction, &QAction::triggered, this, &MainWindow::getServiceSlot);
-    connect(chooseLocationAction, &QAction::triggered, this, &MainWindow::changeLocation);
+  if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    QTextStream entrada(&file);
+    downloadLocation = entrada.readAll().trimmed();
+    file.close();
+    qDebug() << "Download location found:" << downloadLocation;
+  } else {
+    downloadLocation =
+        QStandardPaths::writableLocation(QStandardPaths::DownloadLocation);
+    qDebug() << "file not found. Using: " << downloadLocation;
+  }
 
-    /* Service */
-    downloadPhase = "";
-    service = new Service(this);
+  connect(linkBox, &QLineEdit::textChanged, this,
+          &MainWindow::setDownloadReadiness);
+  connect(getEngineButton, &QPushButton::clicked, this,
+          &MainWindow::getServiceSlot);
+  connect(maintainer, &ServiceMaintainer::started, this,
+          &MainWindow::engineDownloading);
+  connect(maintainer, &ServiceMaintainer::finished, this,
+          &MainWindow::engineDownloaded);
+  connect(chooseStableAction, &QAction::triggered, this,
+          &MainWindow::getServiceSlot);
+  connect(chooseNightlyAction, &QAction::triggered, this,
+          &MainWindow::getServiceSlot);
+  connect(chooseLocationAction, &QAction::triggered, this,
+          &MainWindow::changeLocation);
 
-    connect(downloadButton, &QPushButton::clicked, this, &MainWindow::startDownload);
-    connect(service, &Service::downloadStarted, this, &MainWindow::downloadStarted);
-    connect(service, &Service::downloadFinished, this, &MainWindow::downloadFinished);
-    connect(service, &Service::processFailed, this, &MainWindow::downloadProcessFailed);
-    connect(service, &Service::percentageUpdated, this, &MainWindow::downloadProgress);
-    connect(service, &Service::phaseUpdated, this, &MainWindow::downloadPhaseUpdated);
+  /* Service */
+  downloadPhase = "";
+  service = new Service(this);
+
+  connect(downloadButton, &QPushButton::clicked, this,
+          &MainWindow::startDownload);
+  connect(service, &Service::downloadStarted, this,
+          &MainWindow::downloadStarted);
+  connect(service, &Service::downloadFinished, this,
+          &MainWindow::downloadFinished);
+  connect(service, &Service::processFailed, this,
+          &MainWindow::downloadProcessFailed);
+  connect(service, &Service::percentageUpdated, this,
+          &MainWindow::downloadProgress);
+  connect(service, &Service::phaseUpdated, this,
+          &MainWindow::downloadPhaseUpdated);
 }
 
-void MainWindow::engineDownloading() {
-    statusLabel->setText("Downloading...");
-}
+void MainWindow::engineDownloading() { statusLabel->setText("Downloading..."); }
 
 void MainWindow::engineDownloaded(int exit) {
-    switch (exit) {
-        case 0:
-        statusLabel->setText("yt-dlp downloaded successfully");
-        break;
-        case 1:
-        statusLabel->setText("Download failed: Network error");
-        break;
-        case 2:
-        statusLabel->setText("Download failed: Need permissions to write");
-        break;
-    }
-    setDownloadReadiness();
+  switch (exit) {
+  case 0:
+    statusLabel->setText("yt-dlp downloaded successfully");
+    break;
+  case 1:
+    statusLabel->setText("Download failed: Network error");
+    break;
+  case 2:
+    statusLabel->setText("Download failed: Need permissions to write");
+    break;
+  }
+  setDownloadReadiness();
 }
 
 void MainWindow::setDownloadReadiness() {
-    if (!linkBox->text().isEmpty() && maintainer->exists())
-    {
-        downloadButton->setEnabled(true);
-    }
-    else 
-    {
-        downloadButton->setEnabled(false);
-    }
+  if (!linkBox->text().isEmpty() && maintainer->exists()) {
+    downloadButton->setEnabled(true);
+  } else {
+    downloadButton->setEnabled(false);
+  }
 }
 
 void MainWindow::getServiceSlot() {
-    bool nightly {chooseNightlyAction->isChecked()};
-    maintainer->getService(nightly);
+  bool nightly{chooseNightlyAction->isChecked()};
+  maintainer->getService(nightly);
 }
 
 void MainWindow::changeLocation() {
-    downloadLocation = QFileDialog::getExistingDirectory(
-    this,
-    "Elegí dónde guardar los videos",
-    QDir::homePath(),
-    QFileDialog::ShowDirsOnly
-);
-    if (!downloadLocation.isEmpty()) {
-        QFile file("config.txt");
-        if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-            
-            QTextStream salida(&file);
-            salida << downloadLocation;
-            file.close();
-            qDebug() << "Settings saved.";
+  downloadLocation = QFileDialog::getExistingDirectory(
+      this, "Elegí dónde guardar los videos", QDir::homePath(),
+      QFileDialog::ShowDirsOnly);
+  if (!downloadLocation.isEmpty()) {
+    QFile file("config.txt");
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
 
-        } else {
-            qDebug() << "Error: Couldn't create settings file.";
-        }
+      QTextStream salida(&file);
+      salida << downloadLocation;
+      file.close();
+      qDebug() << "Settings saved.";
 
-        qDebug() << "Chosen folder:" << downloadLocation;
+    } else {
+      qDebug() << "Error: Couldn't create settings file.";
     }
+
+    qDebug() << "Chosen folder:" << downloadLocation;
+  }
 }
 
 void MainWindow::startDownload() {
-    service->startDownload(linkBox->text(), downloadLocation);
+  service->startDownload(linkBox->text(), downloadLocation);
 }
 
 void MainWindow::downloadStarted() {
-    statusLabel->setText("Download started");
-    progressBar->setValue(0);
-    progressBar->show();
+  statusLabel->setText("Download started");
+  progressBar->setValue(0);
+  progressBar->show();
 }
 
 void MainWindow::downloadProgress(int percentage) {
-    statusLabel->setText(downloadPhase + QString::number(percentage) + "%");
-    progressBar->setValue(percentage);
+  statusLabel->setText(downloadPhase + QString::number(percentage) + "%");
+  progressBar->setValue(percentage);
 }
 
 void MainWindow::downloadFinished(int exit) {
-    if (exit == 0) {
-        statusLabel->setText("Download finished successfully");
-    } else if (exit == 1) {
-        statusLabel->setText("Download failed, error code: " + QString::number(exit));
-    }
-    progressBar->hide();
+  if (exit == 0) {
+    statusLabel->setText("Download finished successfully at: " + downloadLocation);
+  } else if (exit == 1) {
+    statusLabel->setText("Download failed, error code: " +
+                         QString::number(exit));
+  }
+  progressBar->hide();
 }
 
 void MainWindow::downloadProcessFailed(QString error) {
-    statusLabel->setText(error);
+  statusLabel->setText(error);
+  progressBar->hide();
 }
 
-void MainWindow::downloadPhaseUpdated(QString phase) {
-    downloadPhase = phase;
-}
+void MainWindow::downloadPhaseUpdated(QString phase) { downloadPhase = phase; }
--- ServiceMaintainer.cpp
+++ ServiceMaintainer.cpp
@@ -1,51 +1,60 @@
 #include "ServiceMaintainer.h"
 
 ServiceMaintainer::ServiceMaintainer(QObject *parent) {
-    downloadProcess = new QProcess(this);
+  downloadProcess = new QProcess(this);
 
-    connect(downloadProcess, &QProcess::started, this, &ServiceMaintainer::started);
-    connect(downloadProcess, &QProcess::finished, this, &ServiceMaintainer::finished);
-    connect(downloadProcess, &QProcess::finished, this, &ServiceMaintainer::onProcessFinish);
+  connect(downloadProcess, &QProcess::started, this,
+          &ServiceMaintainer::started);
+  connect(downloadProcess, &QProcess::finished, this,
+          &ServiceMaintainer::onProcessFinish);
 }
 
 void ServiceMaintainer::getService(bool nightly) {
-    QDir directory;
-    if (!directory.exists(serviceDirectory)) {
-        if (!directory.mkdir(serviceDirectory)) {
-            emit finished(2);
-            return;
-        }
+  QDir directory;
+  if (!directory.exists(serviceDirectory)) {
+    if (!directory.mkdir(serviceDirectory)) {
+      emit finished(2);
+      return;
     }
-    QString command;
-    if (nightly) {
-        command = "wget -O \"" + serviceFile + "\" "
-                          "https://github.com/yt-dlp/yt-dlp-nightly-builds/releases/latest/download/yt-dlp && "
-                          "chmod a+rx \"" + serviceFile + "\"";
-    } else {
-        command = "wget -O \"" + serviceFile + "\" "
-                          "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp && "
-                          "chmod a+rx \"" + serviceFile + "\"";
-    }
+  }
+  QString command;
+  if (nightly) {
+    command = "wget -O \"" + serviceFile +
+              "\" "
+              "https://github.com/yt-dlp/yt-dlp-nightly-builds/releases/latest/"
+              "download/yt-dlp && "
+              "chmod a+rx \"" +
+              serviceFile + "\"";
+  } else {
+    command =
+        "wget -O \"" + serviceFile +
+        "\" "
+        "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp && "
+        "chmod a+rx \"" +
+        serviceFile + "\"";
+  }
 
-    downloadProcess->start("bash", {"-c", command});
+  downloadProcess->start("bash", {"-c", command});
 }
 
-void ServiceMaintainer::onProcessFinish(int exitCode, QProcess::ExitStatus status) {
-    // If process returned 0 (success)
-    if (exitCode == 0 && status == QProcess::NormalExit) {
-        qDebug() << "[ServiceMaintainer] yt-dlp descargado. (wget)";
-        emit finished(0);
-    } else {
-        qDebug() << "[ServiceMaintainer] Fallo en la descarga (wget). Exit code:" << exitCode;
-        emit finished(1);
-    }
+void ServiceMaintainer::onProcessFinish(int exitCode,
+                                        QProcess::ExitStatus status) {
+  // If process returned 0 (success)
+  if (exitCode == 0 && status == QProcess::NormalExit) {
+    qDebug() << "[ServiceMaintainer] yt-dlp descargado. (wget)";
+    emit finished(0);
+  } else {
+    qDebug() << "[ServiceMaintainer] Fallo en la descarga (wget). Exit code:"
+             << exitCode;
+    emit finished(1);
+  }
 }
 
 bool ServiceMaintainer::exists() {
-    return QFile::exists(serviceFile); // Indica si existe el archivo yt-dlp
+  return QFile::exists(serviceFile); // Indica si existe el archivo yt-dlp
 }
 
 QString ServiceMaintainer::getServiceLocation() {
-    QString appPath = QCoreApplication::applicationDirPath();
-    return appPath + "/bin/yt-dlp";
+  QString appPath = QCoreApplication::applicationDirPath();
+  return appPath + "/bin/yt-dlp";
 }
