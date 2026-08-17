46a5064b96a775bde515de93482ad481b228fd97
Maximo Pardo <122894192+maxipardo@users.noreply.github.com>
2026-08-17 10:03:06 -0300

    i18n suppoort, spanish translation complete

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <QApplication>
 #include <QIcon>
+#include <QTranslator>
+#include <QLocale>
 #include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+    QTranslator translator;
+    
+    if (translator.load(QLocale(), "phoca", "_", ":/i18n")) {
+        app.installTranslator(&translator);
+    }
 
     app.setWindowIcon(QIcon(":/phoca.png"));
     app.setDesktopFileName("phoca.desktop");
