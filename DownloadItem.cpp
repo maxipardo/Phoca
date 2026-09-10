be54347c8ecbc4d78f1339d45f4d3a17a9bbe03b
Maximo Pardo <122894192+maxipardo@users.noreply.github.com>
2026-09-10 18:39:45 -0300

    windows delete file icons

--- DownloadItem.cpp
+++ DownloadItem.cpp
@@ -234,9 +234,9 @@ void DownloadItem::contextMenuEvent(QContextMenuEvent *event) {
             deleteIcon = QIcon::fromTheme("edit-delete");
             if (deleteIcon.isNull()) {
                   if (isDarkMode) {
-                        deleteIcon = QIcon(":/cancel_light.svg");
+                        deleteIcon = QIcon(":/delete_light.svg");
                   } else {
-                        deleteIcon = QIcon(":/cancel_dark.svg");
+                        deleteIcon = QIcon(":/delete_dark.svg");
                   }
             }
             deleteFileAction->setIcon(deleteIcon);
